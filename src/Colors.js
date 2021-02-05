8b6226cc1b05aac76518f6715dfd3d250af9b4af
Maneesh <maneeshsriram123@gmail.com>
2021-02-05 23:03:34 +0530

    Create Colors.js

---
+++ src/Colors.js
@@ -0,0 +1,12 @@
+const AppTheme = {
+    light: {
+        backgroundColor: "white",
+        color: "black"
+    },
+    dark: {
+        backgroundColor: "black",
+        color: "white"
+    }
+}
+
+export default AppTheme
