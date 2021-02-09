39f12aa19bad2c8d8279e963c11d89d208a7edf5
Maneesh <maneeshsriram123@gmail.com>
2021-02-09 12:38:40 +0530

    Update ThemeToggler.js

--- src/Components/ThemeToggler.js
+++ src/Components/ThemeToggler.js
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react'
-import ThemeContext from '../Context/ThemeContext'
+import Context from '../Context/ThemeContext'
 
 const ThemeToggler = () => {
-    const [theme, setTheme] = useContext(ThemeContext)
+    const [theme, setTheme] = useContext(Context)
 
     return (
         <div>
+            <h1>{theme} mode</h1>
             <button onClick={() => { setTheme(theme === "light" ? "dark" : "light") }} style={{ padding: "1rem", fontSize: "1.2rem", outline: "none", border: "none", backgroundColor: "yellow" }}>
                 {theme === "light" ? "Switch off the lights" : "Turn on the lights"}
             </button>
