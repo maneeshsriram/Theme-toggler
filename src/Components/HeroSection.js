17b62bb00773203916f8abd32f57f21c27cb0952
Maneesh <maneeshsriram123@gmail.com>
2021-02-09 12:38:43 +0530

    Update HeroSection.js

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,15 +1,15 @@
 import React, { useContext } from 'react'
-import ThemeContext from '../Context/ThemeContext'
+import Context from '../Context/ThemeContext'
 import AppTheme from '../Colors'
 import ThemeToggler from './ThemeToggler'
 
 const HeroSection = () => {
 
-    const theme = useContext(ThemeContext)[0]
+    const [theme] = useContext(Context)
     const currentTheme = AppTheme[theme]
 
     return (
-        <div style={{ textAlign: "center", height: "100vh", padding: "37vh", backgroundColor: `${currentTheme.backgroundColor}`, color: `${currentTheme.color}` }}>
+        <div style={{ textAlign: "center", height: "100vh", padding: "31vh", backgroundColor: `${currentTheme.backgroundColor}`, color: `${currentTheme.color}` }}>
             <ThemeToggler />
             <h1>CONTEXT API THEME-TOGGLER</h1>
             <p>Dark, Light - Theme switching app</p>
